6bdd7a8bbfe5745df895f9c5e8c6d54e95f4f05d
Shirley Aguilar <shirley.aguilar.agui@gmail.com>
2022-07-26 13:23:28 -0500

    añadiendiendo estructura, estilos y funcionalidad al nav

---
+++ src/app/header-nav/nav-header.ts
@@ -0,0 +1,18 @@
+export const navbarData = [
+  {
+      routeLink:'comics',
+      label:'Comics'
+  },
+  {
+      routeLink:'characters',
+      label:'Characters'
+  },
+  {
+      routeLink:'series',
+      label:'Series'
+  },
+  {
+      routeLink:'stories',
+      label:'Stories'
+  }
+]
