a9b660c0e67f3ed630c4abf9b009136297ba5a7c
Shirley Aguilar <shirley.aguilar.agui@gmail.com>
2022-07-26 23:13:47 -0500

    funcionalidad auth y register + styles y vista login-register

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,57 +1,81 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { RestService } from '../shared/services/rest.service';
-/* import { UserService } from '../shared/services/user.service'; */
+ import { UserService } from '../shared/services/user.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
+
 export class LoginComponent implements OnInit {
+  loginForm!: FormGroup ;
   hide = true;
+  login:boolean=false;
   constructor(
     private router: Router,
-    private RestService: RestService,
-   ) { }
+    private UserService: UserService,
+    private formBuilder: FormBuilder,
+  ) { this.buildForm(); }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  private buildForm() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email ], [] ],
+      password: ['', [Validators.required, Validators.minLength(6) ] ],
+    });
+    this.loginForm.valid;
   }
 
-  // usando material design
-  email = new FormControl('', [Validators .required, Validators.email]);
-  password = new FormControl('',)
-
-  getErrorMessage(): "" | "Este campo es obligatorio" | "Correo invalido" {
-    if (this.email.hasError('required')) {
-      return 'Este campo es obligatorio';
-    }
-
-    return this.email.hasError('email') ? 'Correo invalido' : '';
+  get emailField(){
+    return this.loginForm.get('email')
   }
 
-  // capturando valores
+  get passwordField(){
+    return this.loginForm.get('password')
+  }
+
     // guardando datos de autenticacion
-    save(event:Event) {
+  public  save(event:Event) {
       event.preventDefault();
-      if(this.email.valid){
-        console.log(this.email.value, this.password.value);
-        // this.postAuthenticated(this.email.value!, this.password.value!)
-      }
-    }
-
-/*     public postAuthenticated = (email:string, password:string):void => {
-      this.UserService.login(email, password)
-      .subscribe({
-        next: data => {
-          sessionStorage.setItem('token',  JSON.stringify(data.accessToken).replace(/['"]+/g, '')),
-          this.router.navigate(['/gallery']);
-        },
-        error: error => {
-          console.log(JSON.stringify(error));
+      if(this.loginForm.valid){
+        const email = this.loginForm.value.email;
+        const password = this.loginForm.value.password;
+        console.log(this.loginForm.value);
+        if(this.login){
+          this.postAuthenticated(this.loginForm.value)
+        } else{
+          this.postRegister(this.loginForm.value)
         }
-      })
-    } */
+      } else {
+        this.loginForm.markAllAsTouched();
+      }
+  }
 
+  public postAuthenticated = (data:any):void => {
+    this.UserService.login(data)
+    .then(resp => {
+      this.router.navigate(['/gallery'])
+     })
+    .catch(error => console.log(error));
+  }
+
+  public postRegister = (data:any):void => {
+    this.UserService.register(data)
+    .then(resp => {
+     this.router.navigate(['/'])
+     })
+    .catch(error => console.log(error));
+  }
+
+  navigateHome(){
+    this.router.navigate(['/'])
+  }
+
+  clear(){
+    this.loginForm.reset();
+  }
 }
